ec25ede85fc59f69586ac8efc549a9c29d6d93b5
Anna5756855 <dolgopolovaanna5@gmail.com>
2022-11-21 20:49:58 +0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.8cbcf3cccca058621f66b1773e629968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7296cec9f53879d2d05",
+    "url": "/Anna5756855/test-messager/static/css/main.02f528c3.chunk.css"
+  },
+  {
+    "revision": "f7296cec9f53879d2d05",
+    "url": "/Anna5756855/test-messager/static/js/main.f7296cec.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/Anna5756855/test-messager/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "b9cbc3b18aacefd19ebe",
+    "url": "/Anna5756855/test-messager/static/js/2.b9cbc3b1.chunk.js"
+  },
+  {
+    "revision": "8faedf24f56a35fa894f",
+    "url": "/Anna5756855/test-messager/static/js/runtime~main.8faedf24.js"
+  },
+  {
+    "revision": "21fdd85d43b1d804eb28045a5df24e5f",
+    "url": "/Anna5756855/test-messager/index.html"
+  }
+];
