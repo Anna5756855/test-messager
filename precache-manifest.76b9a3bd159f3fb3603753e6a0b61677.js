5ad806fd72069945c561de945cd0a07c1d42498f
Anna5756855 <dolgopolovaanna5@gmail.com>
2022-11-21 21:59:16 +0300

    Updates

---
+++ precache-manifest.76b9a3bd159f3fb3603753e6a0b61677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7296cec9f53879d2d05",
+    "url": "/test-messager/static/css/main.02f528c3.chunk.css"
+  },
+  {
+    "revision": "f7296cec9f53879d2d05",
+    "url": "/test-messager/static/js/main.f7296cec.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/test-messager/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "b9cbc3b18aacefd19ebe",
+    "url": "/test-messager/static/js/2.b9cbc3b1.chunk.js"
+  },
+  {
+    "revision": "3daee5e49621aed2a75e",
+    "url": "/test-messager/static/js/runtime~main.3daee5e4.js"
+  },
+  {
+    "revision": "a45b3f0cab88af90e85b1d24fcbaeba4",
+    "url": "/test-messager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Anna5756855/test-messager/precache-manifest.8cbcf3cccca058621f66b1773e629968.js"
+  "/test-messager/precache-manifest.76b9a3bd159f3fb3603753e6a0b61677.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Anna5756855/test-messager/index.html", {
+workbox.routing.registerNavigationRoute("/test-messager/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
